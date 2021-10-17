c9c9f9fce0aaa7dc028e3f3d7c36e46a9d403fc1
JananiSBabu <janani.rsubramanian@gmail.com>
2021-10-16 17:48:44 -0700

    Customization: Removed hardcoded values from template yaml and added template directives - FRONTEND

--- app/frontend/dockerize.sh
+++ app/frontend/dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ng build --prod 
 docker build  -f Dockerfile -t frontend .
-docker tag frontend phico/frontend:1.1
-docker push phico/frontend:1.1
+docker tag frontend phico/frontend:2.0
+docker push phico/frontend:2.0
